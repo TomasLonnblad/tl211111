f1a59a974b1ba8d4ebf98dc982b56e2333581a91
TomasLonnblad <tomaslonnblad@gmail.com>
2026-03-12 11:13:46 +0100

    x

---
+++ src/app/(main)/dashboard/motion11/app/page.tsx
@@ -0,0 +1,9 @@
+import Component from '../components/shadcn-studio/card/card-17'
+import './globals.css'
+export default function Page() {
+  return (
+    <main className="flex min-h-svh items-center justify-center">
+      <Component />
+    </main>
+  )
+}
