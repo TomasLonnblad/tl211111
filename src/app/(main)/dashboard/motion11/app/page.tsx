c084c6c1e4b861b2218a0ab8705ff11eee7dd124
TomasLonnblad <tomaslonnblad@gmail.com>
2026-03-13 07:31:02 +0100

    x

--- src/app/(main)/dashboard/motion11/app/page.tsx
+++ src/app/(main)/dashboard/motion11/app/page.tsx
@@ -1,9 +1,14 @@
-import Component from '../components/shadcn-studio/card/card-17'
-import './globals.css'
-export default function Page() {
+import { TrashIcon } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+
+const ButtonDeleteDemo = () => {
   return (
-    <main className="flex min-h-svh items-center justify-center">
-      <Component />
-    </main>
+    <Button className='from-destructive via-destructive/60 to-destructive focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 bg-transparent bg-gradient-to-r [background-size:200%_auto] text-white hover:bg-transparent hover:bg-[99%_center]'>
+      <TrashIcon />
+      Delete
+    </Button>
   )
 }
+
+export default ButtonDeleteDemo
